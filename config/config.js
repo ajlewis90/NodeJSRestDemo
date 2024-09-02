0c91d7e6b02dcb036c4ce69dc95ed33fee6af758
Arthur Lewis <arthur.lewis420@gmail.com>
2024-09-03 09:04:57 +1200

    Updated config typo

--- config/config.js
+++ config/config.js
@@ -1,11 +1,9 @@
 let env = process.env.NODE_ENV || 'dev'   // 1
-let mongodb_url = process.env.MONGODB_URI || 'mongodb://localhost:27017/nodejs_test_db;';
+let mongodb_url = process.env.MONGODB_URL || 'mongodb://localhost:27017/nodejs_test_db;';
 // URL to debug flow on test server
 //let mongodb_uri = process.env.MONGODB_URI || 'mongodb://MongoDBTestServerAdmin:Cellfone01@54.206.53.18:27017/LNB-MerchantProfileDB' 
 console.log('the env is ' + env);
 process.env.NODE_ENV  = env;
-process.env.MERCHANT_APPOINTMENT_SVC_USERNAME = process.env.MERCHANT_APPOINTMENT_SVC_USERNAME || 'ajlewis90@live.com'
-process.env.MERCHANT_APPOINTMENT_SVC_PASSWORD = process.env.MERCHANT_APPOINTMENT_SVC_PASSWORD || 'Cellfone01'
 
 //process.env.PORT = 8001;
 //process.env.PORT = 8081;
